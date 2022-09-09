2e713fd2a45cccb19e06589f0071ad8323eb32d0
Anik chakraborty <anikchakraborty562@gmail.com>
2022-09-09 13:18:09 +0600

    Create 2.js

---
+++ Assignment_3/JavaScript function/2.js
@@ -0,0 +1,11 @@
+function fun(str)
+{
+  var array1 = str.split(' ');
+  var newarray1 = [];
+    
+  for(var x = 0; x < array1.length; x++){
+      newarray1.push(array1[x].charAt(0).toUpperCase()+array1[x].slice(1));
+  }
+  return newarray1.join(' ');
+}
+console.log(fun("lowercase changer code"));
