3d544507ed1b2b4fb44e6b6507ca6a527b15fab9
Anik chakraborty <anikchakraborty562@gmail.com>
2022-09-09 12:32:56 +0600

    Create 10.js

---
+++ Assignment_3/JavaScript Basic/10.js
@@ -0,0 +1,14 @@
+function fun(n){
+    let answer = 1;
+    if (n == 0 || n == 1){
+      return answer;
+    }
+    else{
+      for(var i = n; i >= 1; i--){
+        answer = answer * i;
+      }
+      return answer;
+    }  
+  }
+
+  console.log(fun(5));
